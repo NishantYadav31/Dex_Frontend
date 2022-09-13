df998b71bf79e451ef2b145ee1b5163f6053ae3a
NishantYadav31 <nyadav916@gmail.com>
2022-09-13 21:15:47 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Alma Dex.
+        </p>
+        <h3>Build in Progress</h3>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
